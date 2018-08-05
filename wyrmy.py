cf0f2e5574c687ac8e334cae8eb52621c5f791c9
avirut <avirut.mehta@gmail.com>
2018-08-05 18:36:25 -0500

    loads tif image

--- wyrmy.py
+++ wyrmy.py
@@ -1,14 +1,15 @@
 import sys
 
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel
+from PyQt5.QtGui import QIcon, QImageReader
 
 WIDTH = 1500
 HEIGHT = 950
 
 QtCore.QCoreApplication.addLibraryPath('//server/location/PyQt5/plugins')
 app = QApplication(sys.argv)
+
 
 screen_res = app.desktop().screenGeometry()
 screen_width, screen_height = screen_res.width(), screen_res.height()
@@ -18,6 +19,15 @@ w.resize(WIDTH, HEIGHT)
 w.move(int((screen_width/2)-(WIDTH/2)), int(((screen_height-100)/2)-(HEIGHT/2)))
 w.setWindowTitle('Wyrmy')
 w.setWindowIcon(QIcon('resources/wyrmytheworm.png'))
+
+fileName = QFileDialog.getOpenFileName(w, "Open Image", "/", "Image Files (*.png *.jpg *.bmp *.tiff *.tif)")
+img = QtGui.QPixmap(fileName[0])
+
+label = QLabel(w)
+label.setPixmap(img)
+w.resize(img.width(), img.height())
+w.setWindowTitle('Wyrmy - ' + fileName[0])
+
 w.show()
 
 sys.exit(app.exec_())
