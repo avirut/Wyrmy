f228c9dc01ef61fb5e2f4aff30b592da37893fc2
avirut <avirut.mehta@gmail.com>
2018-08-05 19:39:33 -0500

    switch to object oriented + window based scaling

--- wyrmy.py
+++ wyrmy.py
@@ -4,30 +4,46 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel
 from PyQt5.QtGui import QIcon, QImageReader
 
-WIDTH = 1500
-HEIGHT = 950
 
-QtCore.QCoreApplication.addLibraryPath('//server/location/PyQt5/plugins')
-app = QApplication(sys.argv)
+class Wyrmy(QWidget):
+
+    def __init__(self, app):
+        super(Wyrmy, self).__init__()
+
+        # get basic information
+        screen_res = app.desktop().screenGeometry()
+        self.screen_width, self.screen_height = screen_res.width(), screen_res.height()
+
+        # get images to use
+        file_names_import = QFileDialog.getOpenFileNames(self, "Open Image", "/",
+                                                         "Image Files (*.png *.jpg *.bmp *.tiff *.tif)")
+        self.file_names = file_names_import[0]
+
+        # create first image
+        orig = QtGui.QPixmap(self.file_names[0])
+        scale_by = self.screen_height*0.75/orig.height()
+        self.img = orig.scaled(orig.width()*scale_by, orig.height()*scale_by, QtCore.Qt.KeepAspectRatio)
+
+        panel_height = self.screen_height*0.1
+
+        self.width, self.height = self.img.width(), self.img.height()+panel_height
+
+        self.init_win()
+
+        self.show()
+
+    def init_win(self):
+        self.resize(self.width, self.height)
+        self.move(int((self.screen_width / 2) - (self.width / 2)),
+                  int(((self.screen_height - 100) / 2) - (self.height / 2)))
+        self.setWindowIcon(QIcon('resources/wyrmytheworm.png'))
+        label = QLabel(self)
+        label.setPixmap(self.img)
+        self.setWindowTitle('Wyrmy - ' + self.file_names[0])
 
 
-screen_res = app.desktop().screenGeometry()
-screen_width, screen_height = screen_res.width(), screen_res.height()
-
-w = QWidget()
-w.resize(WIDTH, HEIGHT)
-w.move(int((screen_width/2)-(WIDTH/2)), int(((screen_height-100)/2)-(HEIGHT/2)))
-w.setWindowTitle('Wyrmy')
-w.setWindowIcon(QIcon('resources/wyrmytheworm.png'))
-
-fileName = QFileDialog.getOpenFileName(w, "Open Image", "/", "Image Files (*.png *.jpg *.bmp *.tiff *.tif)")
-img = QtGui.QPixmap(fileName[0])
-
-label = QLabel(w)
-label.setPixmap(img)
-w.resize(img.width(), img.height())
-w.setWindowTitle('Wyrmy - ' + fileName[0])
-
-w.show()
-
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    # QtCore.QCoreApplication.addLibraryPath('//server/location/PyQt5/plugins')
+    app = QApplication(sys.argv)
+    window = Wyrmy(app)
+    sys.exit(app.exec_())
